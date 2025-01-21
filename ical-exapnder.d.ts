47351ba5c84dbe513436a9ff077dddc0af5e0206
adobrodiy <13848802+adobrodiy@users.noreply.github.com>
2025-01-21 23:30:47 +0300

    Init

---
+++ ical-exapnder.d.ts
@@ -0,0 +1,27 @@
+declare module 'ical-expander' {
+    class IcalExpander {
+        constructor(opts: any);
+        maxIterations: any;
+        skipInvalidDates: any;
+        jCalData: any;
+        component: any;
+        events: any;
+        between(after: any, before: any): {
+            events: any[];
+            occurrences: any[];
+        };
+        before(before: any): {
+            events: any[];
+            occurrences: any[];
+        };
+        after(after: any): {
+            events: any[];
+            occurrences: any[];
+        };
+        all(): {
+            events: any[];
+            occurrences: any[];
+        };
+    }
+    export default IcalExpander;
+}
