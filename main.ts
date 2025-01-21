f2e95d0f6bca8150de88b313c26b057fc7c28590
adobrodiy <13848802+adobrodiy@users.noreply.github.com>
2025-01-22 01:55:39 +0300

    improved much a modal

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, requestUrl } from 'obsidian';
+import { App, ButtonComponent, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, requestUrl } from 'obsidian';
 import IcalExpander from 'ical-expander';
 
 interface Settings {
@@ -33,10 +33,10 @@ export default class CTTKCPlugin extends Plugin {
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'cttkc-open-calculate-modal',
-			name: 'CTTKC: calculate',
+			name: 'calculate',
 			callback: () => {
-				// new CTTKCModal(this.app).open();
-				loadCalendar(this);
+				new CTTKCModal(this).open();
+				// loadCalendar(this);
 			}
 		});
 
@@ -54,18 +54,164 @@ export default class CTTKCPlugin extends Plugin {
 }
 
 class CTTKCModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	private plugin: CTTKCPlugin;
+	private options: {
+		calendarUrl: string;
+		tasksPrefix: string;
+		startDate: string;
+		endDate: string;
+	};
+	private status: {
+		code: string;
+		message: string;
+	};
+	private statusSetting: Setting;
+	private submitButton: ButtonComponent;
+
+	constructor(plugin: CTTKCPlugin) {
+		super(plugin.app);
+		this.plugin = plugin;
+
+		this.setTitle('CTTKC: calculate');
+
+		this.options = {
+			calendarUrl: plugin.settings.calendarUrl,
+			tasksPrefix: plugin.settings.tasksPrefix,
+			startDate: '',
+			endDate: ''
+		};
+
+		new Setting(this.contentEl)
+			.setName('Calendar url')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your calendar url')
+				.setValue(plugin.settings.calendarUrl)
+				.onChange(async (value) => {
+					this.options.calendarUrl = value;
+					this.validateInputs();
+					this.updateStatus();
+				}));
+
+		new Setting(this.contentEl)
+			.setName('Tasks prefix')
+			.addText(text => text
+				.setPlaceholder('Enter your tasks prefix')
+				.setValue(plugin.settings.tasksPrefix)
+				.onChange(async (value) => {
+					this.options.tasksPrefix = value;
+					this.validateInputs();
+					this.updateStatus();
+				}));
+
+		new Setting(this.contentEl)
+			.setName('Start date')
+			.addText(text => {
+				text
+					.setPlaceholder('Choose start date')
+					.setValue('')
+					.onChange(async (value) => {
+						this.options.startDate = value;
+						console.log('options', this.options);
+						this.validateInputs();
+						this.updateStatus();
+					});
+				text.inputEl.setAttribute('type', 'date');
+			});
+
+		new Setting(this.contentEl)
+			.setName('End date')
+			.addText(text => {
+				text
+					.setPlaceholder('Choose end date')
+					.setValue('')
+					.onChange(async (value) => {
+						this.options.endDate = value;
+						console.log('options', this.options);
+						this.validateInputs();
+						this.updateStatus();
+					});
+				text.inputEl.setAttribute('type', 'date');
+			});
+
+		this.statusSetting = new Setting(this.contentEl)
+			.addButton((btn) => {
+				this.submitButton = btn;
+				btn
+					.setButtonText('Submit')
+					.setCta()
+					.onClick(async () => {
+						// this.close();
+						console.log('options', this.options);
+						// onSubmit(name);
+						this.status = {
+							code: 'loading',
+							message: 'Loading...'
+						};
+						this.updateStatus();
+						await loadCalendar(plugin, this.options);
+						this.status = {
+							code: 'loaded',
+							message: 'Loaded'
+						};
+						this.updateStatus();
+					});
+			});
+
+		this.validateInputs();
+		this.updateStatus();
+	}
+
+	private validateInputs() {
+		if (!this.options.calendarUrl) {
+			this.status = {
+				code: 'error',
+				message: 'Calendar url is required'
+			};
+		} else if (!this.options.startDate) {
+			this.status = {
+				code: 'error',
+				message: 'Start date is required'
+			};
+		} else if (!this.options.endDate) {
+			this.status = {
+				code: 'error',
+				message: 'End date is required'
+			};
+		} else if (this.options.startDate >= this.options.endDate) {
+			this.status = {
+				code: 'error',
+				message: 'Start date should be earlier than end date'
+			};
+		} else {
+			this.status = {
+				code: 'ok',
+				message: 'Ready to calculate'
+			};
+		}
+	}
+	private updateStatus() {
+		this.statusSetting.setHeading().setName(this.status.message);
+		if (['error', 'loading'].includes(this.status.code)) {
+			this.submitButton.setDisabled(true);
+		} else if (['ok', 'loaded'].includes(this.status.code)) {
+			this.submitButton.setDisabled(false);
+		}
+		
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		// contentEl.setText('Woah!');
+
+		console.log('Modal opened');
 	}
 
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
+
+		console.log('Modal closed');
 	}
 }
 
@@ -105,11 +251,16 @@ class SettingTab extends PluginSettingTab {
 	}
 }
 
-async function loadCalendar(plugin: CTTKCPlugin) {
+async function loadCalendar(plugin: CTTKCPlugin, options: {
+	calendarUrl: string;
+	tasksPrefix: string;
+	startDate: string;
+	endDate: string;
+}) {
 	console.log('Started loading calendar');
 
-	const taskPrefix = plugin.settings.tasksPrefix;
-	const calendarUrl = plugin.settings.calendarUrl;
+	const taskPrefix = options.tasksPrefix;
+	const calendarUrl = options.calendarUrl;
 	if (!calendarUrl) {
 		console.log('Calendar url have to be configured');
 		return false;
@@ -132,12 +283,7 @@ async function loadCalendar(plugin: CTTKCPlugin) {
 		ics: data,
 		maxIterations: 0
 	})); }));
-	// // const now = new Date();
-	// // const weekAgo = new Date();
-	// // weekAgo.setDate(weekAgo.getDate() - 7);
-	const { events } = icalExpander.between(new Date('2024-12-23T00:00:00.000Z'), new Date('2024-12-29T00:00:00.000Z'));
-	// const events = icalExpander.between(new Date('2024-12-23T00:00:00.000Z'), new Date('2024-12-29T00:00:00.000Z'));
-	// console.log('events', events);
+	const { events } = icalExpander.between(new Date(`${options.startDate}T00:00:00.000`), new Date(`${options.endDate}T00:00:00.000`));
 
 	const eventAggData: {
 		[key: string]: {
